447847e09404db6e5d94237b72bdade88d463e86
krbyhome <367555@niuitmo.ru>
2023-10-08 02:35:20 +0300

    feat : дефлекторы

---
+++ src/Lab1/Entities/Deflectors/Class1Deflector.cs
@@ -0,0 +1,9 @@
+namespace Itmo.ObjectOrientedProgramming.Lab1.Entities.Deflectors;
+
+public class Class1Deflector : Deflector
+{
+    public Class1Deflector()
+        : base(200)
+    {
+    }
+}
---
+++ src/Lab1/Entities/Deflectors/Class3Deflector.cs
@@ -0,0 +1,9 @@
+namespace Itmo.ObjectOrientedProgramming.Lab1.Entities.Deflectors;
+
+public class Class3Deflector : Deflector
+{
+    public Class3Deflector()
+        : base(4000)
+    {
+    }
+}
